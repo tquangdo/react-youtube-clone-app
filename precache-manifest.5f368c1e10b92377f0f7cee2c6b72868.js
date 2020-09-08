a8c6f5a2b398806ab2f7e2fa7e26dfbeb406371e
tquangdo <trangia61@gmail.com>
2020-09-08 23:20:07 +0900

    Updates

---
+++ precache-manifest.5f368c1e10b92377f0f7cee2c6b72868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d929cdb8cc6393f47a4f520f74542a8d",
+    "url": "/youtube-clone-app/index.html"
+  },
+  {
+    "revision": "0d6edfdbfebeaa175b63",
+    "url": "/youtube-clone-app/static/css/main.1987206f.chunk.css"
+  },
+  {
+    "revision": "9c970af4e89fed5a3e2f",
+    "url": "/youtube-clone-app/static/js/2.4e61ef24.chunk.js"
+  },
+  {
+    "revision": "2d7a7b40e6adfbcca0d182879bd9ce91",
+    "url": "/youtube-clone-app/static/js/2.4e61ef24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6edfdbfebeaa175b63",
+    "url": "/youtube-clone-app/static/js/main.e905727e.chunk.js"
+  },
+  {
+    "revision": "4a46654571db88cd352a",
+    "url": "/youtube-clone-app/static/js/runtime-main.f86beecd.js"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/youtube-clone-app/static/media/yt_logo_rgb_light.07b8560a.png"
+  }
+]);
